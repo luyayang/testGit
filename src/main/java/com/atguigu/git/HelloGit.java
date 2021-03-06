07f4425dd74c5affe00e5098d822ff4fbe165854
LuZong888 <17608781147@163.com>
2021-03-06 17:39:13 +0800

    第二次提交之前

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -1,17 +1,11 @@
 package com.atguigu.git;
-
 public class HelloGit {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
-
 		System.out.println("更新1");
 		System.out.println("创建分支");
-
 		System.out.println("主干添加");
-
 		System.out.println("分支添加");
-		System.out.println("第二次提交之前");}
-
+		System.out.println("第二次提交之前");
+		}
 }

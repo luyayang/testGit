4518a57b03e94e61936251d55ba30b955f0b51a2
luyayang <77443734+luyayang@users.noreply.github.com>
2021-03-06 22:58:16 +0800

    在线更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -1,25 +1,14 @@
 package com.atguigu.git;
 public class HelloGit {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-
-		System.out.println("Hello Git!");
-		System.out.println("更新1");
-		System.out.println("创建分支");
-		System.out.println("主干添加");
-		System.out.println("分支添加");		
-		System.out.println("第二次更新之前");}
-		}
-
-=======
-
 		System.out.println("Hello Git!");
 		System.out.println("鏇存柊1");
 		System.out.println("鍒涘缓鍒嗘敮");
 		System.out.println("涓诲共娣诲姞");
 		System.out.println("鍒嗘敮娣诲姞");		
-		System.out.println("鍦ㄧ嚎鏇存柊");}
+		System.out.println("鍦ㄧ嚎鏇存柊");
+		}
 		}
 
->>>>>>> branch 'master' of https://github.com/luyayang/testGit.git
+
 
